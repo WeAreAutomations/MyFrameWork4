bb8fd61a1cadc72c2ce4aef9300f96215d7f1b05
New1 <angelaborets@gmail.com>
2023-05-26 22:59:06 +0300

    2 теста пройдено додано гілку Angel Create_Account_pageAll

--- src/test/java/tests/createAccountPageTests/Tests.java
+++ src/test/java/tests/createAccountPageTests/Tests.java
@@ -10,67 +10,30 @@ public class Tests extends BaseTest {
         super("win_chrome");
     }
 
-//    @Test
-    public void test1() throws IOException, InterruptedException {
-        //Open a page for test
+    @Test
+    public void Create_Account_pageHead() throws IOException, InterruptedException {
+
         action.openPage("https://dex-trade.com/sign-up");
+        action.waitElementIsVisible("//a[@data-test-id='header signup']");
+        pixelPerfect.byElement("//div[@class=\"header-wrapper\"]", "Screenshots/Create_Account_page/EnChrome/Create_Account_pageHead/real_life_screenshot.png",
+                "Screenshots/Create_Account_page/EnChrome/Create_Account_pageHead/mock_screenshot.png",
+                "Screenshots/Create_Account_page/EnChrome/Create_Account_pageHead/difference.png");
 
-        //Waiting when the page is completely loaded
-        action.waitElementIsVisible("//a[@data-test-id='header login']");
 
-        //Input a text into the selected element
-        action.InputTextByXpath("//input[@type='email']", "yanautomte.st.6@gmail.com");
-
-        //Input a text to the next element
-        action.InputTextByXpath("//input[@type='password']", "Vinnitsa-2022");
-
-        //Perform a click on the element
-        action.clickByXpath("//button[@data-test-id='viewBox']");
-
-        //Run the Pixel perfect test for a selected element
-        pixelPerfect.byElement("(//div[@class='auth-container'])[3]",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test1/real_life_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test1/mock_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test1/difference.png");
-    }
-
-//    @Test
-    public void test2() throws IOException, InterruptedException {
-        action.openPage("https://dex-trade.com/sign-up");
-        action.waitElementIsVisible("//a[@data-test-id='header login']");
-        pixelPerfect.fullPage("Screenshots/Create_Account_page/EnChrome/Screenshots_test2/real_life_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test2/mock_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test2/difference.png");
-//        pixelPerfect.Assert(0.005);
-    }
-
-//    @Test
-    public void test3() throws IOException, InterruptedException {
-        action.openPage("https://dex-trade.com/sign-up");
-        action.waitElementIsVisible("//a[@data-test-id='header login']");
-        action.hoverOverAnElementByXpath("(//span[@class=\"nav-title\"])[2]");
-        pixelPerfect.byElement("//div[@class='menu-container']", "Screenshots/Create_Account_page/EnChrome/Screenshots_test3/real_life_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test3/mock_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test3/difference.png");
-    }
-
-//    @Test
-    public void test4() throws IOException, InterruptedException {
-        action.openPage("https://dex-trade.com/sign-up");
-        action.waitElementIsVisible("//a[@data-test-id='header login']");
-        pixelPerfect.byElement("//div[@class='auth-wrapper header-border']", "Screenshots/Create_Account_page/EnChrome/Screenshots_test4/real_life_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test4/mock_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/Screenshots_test4/difference.png");
     }
 
     @Test
-    public void restore_password() throws IOException, InterruptedException {
-        action.openPage("https://dex-trade.com/restore-password");
+    public void Create_Account_pagePart1() throws IOException, InterruptedException {
+
+        action.openPage("https://dex-trade.com/sign-up");
         action.waitElementIsVisible("//a[@data-test-id='header signup']");
-        pixelPerfect.fullPage("Screenshots/Create_Account_page/EnChrome/restore_password/real_life_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/restore_password/mock_screenshot.png",
-                "Screenshots/Create_Account_page/EnChrome/restore_password/difference.png" );
+        pixelPerfect.byElement("//div[@class=\"ds-container-fluid sign-page-container custom-sign-up-container\"]", "Screenshots/Create_Account_page/EnChrome/Create_Account_pagePart1/real_life_screenshot.png",
+                "Screenshots/Create_Account_page/EnChrome/Create_Account_pagePart1/mock_screenshot.png",
+                "Screenshots/Create_Account_page/EnChrome/Create_Account_pagePart1/difference.png");
+
+
     }
 
 }
+
 
