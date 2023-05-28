b37b0746d0401330121df466c148fb40532f292d
MoToR <alex.bublik.82@gmail.com>
2023-05-29 00:45:20 +0300

    Add tradeNavSpotTradingPage on AllPageChromeTests

--- src/test/java/tests/spotTradingPageTests/AllPageChromeTests.java
+++ src/test/java/tests/spotTradingPageTests/AllPageChromeTests.java
@@ -29,4 +29,22 @@ public class AllPageChromeTests extends BaseTest {
                 "Screenshots/Spot_Trading_page/EnChrome/Screenshots_allHeaderSpotTradingPage/difference.png", "FULL HD");
 
     }
+
+    @Test
+    public void tradeNavSpotTradingPage() throws IOException, InterruptedException {
+        //Open a page for test
+        action.openPage("https://dex-trade.com/spot/trading/BTCUSDT");
+        //Waiting when the page is completely loaded
+        action.waitElementIsVisible("//a[@data-test-id='header login']");
+        //Click button "Accept" use of cookies
+        action.ifButtonIsPresentClick("//button[@class='close-button']");
+        //Hover
+        action.hoverOverAnElementByXpath("(//li[@class='header-nav-item item-relative'])[1]");
+        //Run the Pixel perfect test for a selected element
+        pixelPerfect.byElement("(//div[@class='menu-wrapper'])[1]",
+                "Screenshots/Spot_Trading_page/EnChrome/Screenshots_tradeNavSpotTradingPage/real_life_screenshot.png",
+                "Screenshots/Spot_Trading_page/EnChrome/Screenshots_tradeNavSpotTradingPage/mock_screenshot.png",
+                "Screenshots/Spot_Trading_page/EnChrome/Screenshots_tradeNavSpotTradingPage/difference.png", "FULL HD");
+
+    }
 }
